1b0ed54f5484ca1cdd2115ce1b33d3dc78a404f5
Sai Raj <sairockz05@gmail.com>
2018-02-15 08:00:46 +0530

    removed js files

--- Assignment2/assignMiddleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(router){
-    
-    router.use(function(req,res,next){
-        var today = new Date();
-        console.log("Viewed today at: "+today);
-        next();
-    });
-    
-}
