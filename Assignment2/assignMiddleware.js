05fb0a16ec3dd3d62171b15c30e913b7ae7eee18
Sai Raj <sairockz05@gmail.com>
2018-02-15 08:25:24 +0530

    changed

--- Assignment2/assignMiddleware.js
+++ Assignment2/assignMiddleware.js
@@ -2,12 +2,8 @@ module.exports = function(router){
     
     router.use(function(req,res,next){
         var today = new Date();
-        console.log("All blogs are seen Today at: "+today);
+        console.log("Viewed today at: "+today);
         next();
     });
     
-    router.get('*',function(req,res,next){
-        res.render("Please check the path");
-        next();
-    });
 }
